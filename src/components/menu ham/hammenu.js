b91510e707ff6a7d88e77d1872ac9cc7e36a7799
DanielPRdgz <prietorodriguezdaniel@gmail.com>
2024-03-15 13:16:40 +0100

    commit

--- src/components/menu ham/hammenu.js
+++ src/components/menu ham/hammenu.js
@@ -1,5 +1,9 @@
 import './hammenu.css'
 const menuham = document.querySelector('.menuham')
+const quit = document.createElement('img')
+quit.src =
+  'https://res.cloudinary.com/dj1axfhui/image/upload/v1710496353/marca-de-la-cruz_pzohdu.png'
+quit.classList.add('quitButton')
 export const createMenuHam = () => {
   const menuham = document.querySelector('.menuham')
   const hams = ['User', 'Notifications', 'Settings', 'Log out', 'FAQ']
@@ -8,19 +12,29 @@ export const createMenuHam = () => {
   const ulHam = document.createElement('ul')
   ulHam.classList.add('ulHam')
   for (const item of hams) {
+    const a = document.createElement('a')
+    a.href = '#'
     const li = document.createElement('li')
     li.textContent = item
-    ulHam.appendChild(li)
+    a.appendChild(li)
+    ulHam.appendChild(a)
     navHam.appendChild(ulHam)
   }
+  menuham.appendChild(quit)
   menuham.appendChild(navHam)
 }
 const botonHam = document.createElement('img')
 botonHam.classList.add('botonHam')
-botonHam.classList.add('botonOpacity')
+
 botonHam.src =
   'https://res.cloudinary.com/dj1axfhui/image/upload/v1710446966/barra-de-menus_wk9oco.png'
 document.body.appendChild(botonHam)
 botonHam.addEventListener('click', () => {
-  menuham.classList.toggle('menuham')
+  menuham.classList.toggle('visible')
+  botonHam.classList.remove('botonHam')
+})
+
+quit.addEventListener('click', () => {
+  menuham.classList.remove('visible')
+  botonHam.classList.toggle('botonHam')
 })
